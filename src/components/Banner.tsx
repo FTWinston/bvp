1b6b5a2029b7d7fef273b9a44e6ca973cd73404c
FTWinston <ftwinston@gmail.com>
2019-04-26 23:52:16 +0100

    Banner text and buttons are now configurable

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,37 +2,66 @@ import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Title from './Title';
+import Button from './Button';
+import './Banner.scss';
 
-const BigImage = () => (
+const Banner = () => (
   <StaticQuery
-    query={graphql`
-      query {
-        image: file(relativePath: { eq: "banner.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 720) {
-              ...GatsbyImageSharpFluid
+  query={graphql`
+    query {
+      summary: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex:"/.*/fixed/intro.md/" } },
+        limit: 1
+      )
+      {
+        nodes {
+          frontmatter {
+            buttons {
+              text
+              target
             }
+          }
+          html
+        }
+      }
+      
+      image: file(relativePath: { eq: "banner.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 720) {
+            ...GatsbyImageSharpFluid
           }
         }
       }
-    `}
-    render={data => <Img fluid={data.image.childImageSharp.fluid} className="image" />}
-  />
-)
+    }
+  `}
+  render={data => {
+    const summary = data.summary.nodes[0];
+    const image = data.image;
 
-const Banner = () => (
-  <section className="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
-    <div className="content">
-      <Title />
-      
-      <p className="major">A (modular, highly tweakable) responsive one-page template designed by <a href="https://html5up.net">HTML5 UP</a> and released for free under the <a href="https://html5up.net/license">Creative Commons</a>.</p>
-      <ul className="actions stacked">
-        <li><a href="#first" className="button big wide">Get Started</a></li>
+    const content = <div dangerouslySetInnerHTML={{ __html: summary.html }} />
+
+    const buttons = summary.frontmatter.buttons == null
+        ? undefined
+        : <ul className="actions">
+        {summary.frontmatter.buttons.map((btn, index: number) =>
+          <Button key={index} target={btn.target}>{btn.text}</Button>
+        )}
       </ul>
-    </div>
-    
-    <BigImage />
-  </section>
+
+    return (
+      <section className="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+        <div className="content">
+          <Title />
+          
+          {content}
+          {buttons}
+        </div>
+        
+        <Img fluid={image.childImageSharp.fluid} className="image" />
+      </section>
+    )}
+  }
+/>
 )
 
 export default Banner
