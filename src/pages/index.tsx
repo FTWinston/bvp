00f20e41d130dde4b6b41f8b51ec25ce53a6e301
FTWinston <ftwinston@gmail.com>
2019-04-29 23:38:12 +0100

    Removed commented-out elements from home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,6 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import ContactForm from '../components/ContactForm'
-import Gallery from '../components/Gallery'
-import Grid from '../components/Grid'
 import AllHomepageSections from '../components/AllHomepageSections';
 
 export default () => (
@@ -13,19 +11,6 @@ export default () => (
     
     <AllHomepageSections />
 
-{/*
-    <Spotlight orient="right" image="assets/images/spotlight01.jpg" id="first">
-      <h2>Magna etiam feugiat</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi id ante sed ex pharetra lacinia sit amet vel massa. Donec facilisis laoreet nulla eu bibendum. Donec ut ex risus. Fusce lorem lectus, pharetra pretium massa et, hendrerit vestibulum odio lorem ipsum dolor sit amet.</p>
-      <ul className="actions stacked">
-        <li><a href="assets/images/spotlight01.jpg" className="button">Learn More</a></li>
-      </ul>
-    </Spotlight>
-
-    <Gallery />
-
-    <Grid />
-*/}
     <ContactForm />
   </Layout>
 )
