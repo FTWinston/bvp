7ce078349e742a167f405f564ca9d998f5fa2090
FTWinston <ftwinston@gmail.com>
2019-05-13 00:03:45 +0100

    Button now links directly to download of latest magazine, and shows its name

---
+++ src/components/HeraldButton.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+interface IProps {
+  text: string;
+}
+
+const HeraldButton = (props: IProps) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex:"/.*/herald/.*/" } },
+          sort: { fields: [frontmatter___date], order: DESC },
+          limit: 1
+        )
+        {
+          nodes {
+            frontmatter{
+              title
+              pdf
+            }
+          }
+        }
+      }
+    `}
+    render={data => {
+      const node = data.allMarkdownRemark.nodes[0];
+      return (
+      <li>
+        <a href={node.frontmatter.pdf} className="button big wide">
+          {props.text} ({node.frontmatter.title})
+        </a>
+      </li>
+      )
+    }}
+  />
+)
+
+export default HeraldButton
