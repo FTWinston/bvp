fb26ece803b00787f724f7a3169cc7f08f6d2560
FTWinston <ftwinston@gmail.com>
2020-10-31 16:55:58 +0000

    Added ability to list recordings, and link directly to the most recent recording.

---
+++ src/components/RecordingArchive.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+
+const RecordingArchive = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex:"/.*/recordings/.*/" } },
+          sort: { fields: [frontmatter___date], order: DESC },
+          limit: 6
+        )
+        {
+          nodes {
+            frontmatter{
+              date
+              file
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <div className="items style1 medium onscroll-fade-in">
+        {data.allMarkdownRemark.nodes.map((node, index: number) => (
+          <a href={node.frontmatter.file} key={index}>
+            <span className="icon style2 major fa-save"></span>
+            <p className="major">
+              {node.frontmatter.date}
+            </p>
+          </a>
+        ))}
+      </div>
+    )}
+  />
+)
+
+export default RecordingArchive
